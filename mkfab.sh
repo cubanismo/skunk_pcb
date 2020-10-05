50558f42665c526709a8196548531b904e5d20dd
James Jones <atari@theinnocuous.com>
2020-10-04 19:05:08 -0700

    Finalize Rev. 5 RC1 - It's off to manufacturing!
    -Add Osh Park naming scheme support (also much
     closer to that supported by GerbView) to
     mkfab.sh. Use the '--osh' command line parameter
     to choose it.
    
    -Renamed thruhole.tap skunk_v5.tap in the original
     naming scheme for consistency.
    
    -Removed 'https://' from github URL on the front
     silkscreen. It allows the rest of the text to be
     much larger/more legible, and is really only
     necessary for very pedantic URL typers these
     days.
    
    -Tweaked two traces on the board that were found
     to be slightly askew during final review in
     GerbView.
    
    -Lowered board outline line width to 5mils to
     match FreePCB example config screenshots on
     Osh Park website.
    
    -Archived the exact files sent to Osh Park in
     releases/skunk_v5-rc1-oshpark.zip.
    
    -Stopped saving intermediate release packaging
     files in revision5/* and add that path to
     .gitignore

--- mkfab.sh
+++ mkfab.sh
@@ -2,19 +2,51 @@
 WORKDIR="`dirname $0`"
 WORKDIR="`readlink -f "$WORKDIR"`"
 
+if [ x"$1" = x"--osh" ]; then
+	BOTTOM_SILK_EXT=gbo
+	BOTTOM_SOLDER_EXT=gbs
+	BOTTOM_COPPER_EXT=gbl
+	INNER_2_EXT=g3l
+	INNER_1_EXT=g2l
+	TOP_COPPER_EXT=gtl
+	TOP_SOLDER_EXT=gts
+	TOP_SILK_EXT=gto
+	DRILL_EXT=xln
+	BOARD_OUTLINE_EXT=gko
+else
+	BOTTOM_SILK_EXT=ssb
+	BOTTOM_SOLDER_EXT=smb
+	BOTTOM_COPPER_EXT=bot
+	INNER_2_EXT=in2
+	INNER_1_EXT=in1
+	TOP_COPPER_EXT=top
+	TOP_SOLDER_EXT=smt
+	TOP_SILK_EXT=sst
+	DRILL_EXT=tap
+	BOARD_OUTLINE_EXT=fab
+fi
+
 mkdir -p "$WORKDIR/revision5"
 cd "$WORKDIR/revision5"
 
-cp ../CAM/back_silk.grb skunk_v5.ssb
-cp ../CAM/bottom_solder_mask.grb skunk_v5.smb
-cp ../CAM/bottom_copper.grb skunk_v5.bot
-cp ../CAM/inner_copper_2.grb skunk_v5.in2
-cp ../CAM/inner_copper_1.grb skunk_v5.in1
-cp ../CAM/top_copper.grb skunk_v5.top
-cp ../CAM/top_solder_mask.grb skunk_v5.smt
-cp ../CAM/top_silk.grb skunk_v5.sst
-cp ../CAM/drill_file.drl thruhole.tap
-cp ../CAM/board_outline.grb skunk_v5.fab
+cp ../CAM/back_silk.grb skunk_v5.$BOTTOM_SILK_EXT
+cp ../CAM/bottom_solder_mask.grb skunk_v5.$BOTTOM_SOLDER_EXT
+cp ../CAM/bottom_copper.grb skunk_v5.$BOTTOM_COPPER_EXT
+cp ../CAM/inner_copper_2.grb skunk_v5.$INNER_2_EXT
+cp ../CAM/inner_copper_1.grb skunk_v5.$INNER_1_EXT
+cp ../CAM/top_copper.grb skunk_v5.$TOP_COPPER_EXT
+cp ../CAM/top_solder_mask.grb skunk_v5.$TOP_SOLDER_EXT
+cp ../CAM/top_silk.grb skunk_v5.$TOP_SILK_EXT
+cp ../CAM/drill_file.drl skunk_v5.$DRILL_EXT
+cp ../CAM/board_outline.grb skunk_v5.$BOARD_OUTLINE_EXT
 rm -f skunk_v5.zip
-zip skunk_v5.zip *
-
+zip skunk_v5.zip skunk_v5.$BOTTOM_SILK_EXT
+zip skunk_v5.zip skunk_v5.$BOTTOM_SOLDER_EXT
+zip skunk_v5.zip skunk_v5.$BOTTOM_COPPER_EXT
+zip skunk_v5.zip skunk_v5.$INNER_2_EXT
+zip skunk_v5.zip skunk_v5.$INNER_1_EXT
+zip skunk_v5.zip skunk_v5.$TOP_COPPER_EXT
+zip skunk_v5.zip skunk_v5.$TOP_SOLDER_EXT
+zip skunk_v5.zip skunk_v5.$TOP_SILK_EXT
+zip skunk_v5.zip skunk_v5.$DRILL_EXT
+zip skunk_v5.zip skunk_v5.$BOARD_OUTLINE_EXT

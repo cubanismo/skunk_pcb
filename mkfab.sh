5a7c54fd20fdfb0499e5e8bdf3a9882196305816
James Jones <atari@theinnocuous.com>
2020-10-05 22:29:05 -0700

    Small cleanups to back silkscreen + release tools
    -Checked in version 5 back/bottom silkscreen
     files:
     -back_silk_v5.svg - The pre-processed silkscreen
      SVG ready for conversion.
     -back_silk_v5-fab3000.gbr - The final silkscreen
      gerber file produced using FAB 3000
     -back_silk_v5-svg2shenzen_kicad.gbr - The final
      silkscreen gerber file produced using
      svg2shenzen and KiCad.
    
    -Updated notes to include method to successfully
     generate a silkscreen using FAB 3000.
    
    -mkfab.sh: Added option to switch between
     svg2shenzen+KiCad and fab3000 bottom silkscreen
     gerber file.
    
    -mkfab.sh: Added support for the elecrow gerber
     file naming scheme.
    
    -Checked in a FreePCB report file from the RC1
     files.

--- mkfab.sh
+++ mkfab.sh
@@ -2,51 +2,98 @@
 WORKDIR="`dirname $0`"
 WORKDIR="`readlink -f "$WORKDIR"`"
 
-if [ x"$1" = x"--osh" ]; then
-	BOTTOM_SILK_EXT=gbo
-	BOTTOM_SOLDER_EXT=gbs
-	BOTTOM_COPPER_EXT=gbl
-	INNER_2_EXT=g3l
-	INNER_1_EXT=g2l
-	TOP_COPPER_EXT=gtl
-	TOP_SOLDER_EXT=gts
-	TOP_SILK_EXT=gto
-	DRILL_EXT=xln
-	BOARD_OUTLINE_EXT=gko
-else
-	BOTTOM_SILK_EXT=ssb
-	BOTTOM_SOLDER_EXT=smb
-	BOTTOM_COPPER_EXT=bot
-	INNER_2_EXT=in2
-	INNER_1_EXT=in1
-	TOP_COPPER_EXT=top
-	TOP_SOLDER_EXT=smt
-	TOP_SILK_EXT=sst
-	DRILL_EXT=tap
-	BOARD_OUTLINE_EXT=fab
-fi
+# Edit this if you bump the board version number!
+REVISION=5
+
+BOTTOM_SILK_EXT=ssb
+BOTTOM_SOLDER_EXT=smb
+BOTTOM_COPPER_EXT=bot
+INNER_2_EXT=in2
+INNER_1_EXT=in1
+TOP_COPPER_EXT=top
+TOP_SOLDER_EXT=smt
+TOP_SILK_EXT=sst
+DRILL_EXT=tap
+BOARD_OUTLINE_EXT=fab
+
+BOTTOM_SILK_SRC=../releases/back_silk_v$REVISION-svg2shenzen_kicad.gbr
+
+while [ $# -gt 0 ]; do
+	if [ x"$1" = x"--osh" ]; then
+		echo "Using OSH PARK layer naming scheme"
+		BOTTOM_SILK_EXT=gbo
+		BOTTOM_SOLDER_EXT=gbs
+		BOTTOM_COPPER_EXT=gbl
+		INNER_2_EXT=g3l
+		INNER_1_EXT=g2l
+		TOP_COPPER_EXT=gtl
+		TOP_SOLDER_EXT=gts
+		TOP_SILK_EXT=gto
+		DRILL_EXT=xln
+		BOARD_OUTLINE_EXT=gko
+	elif [ x"$1" = x"--elecrow" ]; then
+		echo "Using Elecrow layer naming scheme"
+		BOTTOM_SILK_EXT=gbo
+		BOTTOM_SOLDER_EXT=gbs
+		BOTTOM_COPPER_EXT=gbl
+		INNER_2_EXT=g2
+		INNER_1_EXT=g1
+		TOP_COPPER_EXT=gtl
+		TOP_SOLDER_EXT=gts
+		TOP_SILK_EXT=gto
+		DRILL_EXT=txt
+		BOARD_OUTLINE_EXT=gml
+	elif [ x"$1" = x"--fab3000" ]; then
+		BOTTOM_SILK_SRC=../releases/back_silk_v$REVISION-fab3000.gbr
+	else
+		echo "Invalid parameter: $1"
+		echo "Usage: mkfab.sh [--osh] [--fab3000]"
+		echo ""
+		echo "  --osh: Use OSH PARK gerber file name extension scheme"
+		echo "  --fab3000: Use the fab3000 back silkscreen instead of "
+                echo "             the svg2shenzen + KiCad one"
+		exit 1
+	fi
+
+	shift
+done
+
+echo "Using '`basename $BOTTOM_SILK_SRC`' as bottom silk screen layer"
 
 mkdir -p "$WORKDIR/revision5"
 cd "$WORKDIR/revision5"
 
-cp ../CAM/back_silk.grb skunk_v5.$BOTTOM_SILK_EXT
-cp ../CAM/bottom_solder_mask.grb skunk_v5.$BOTTOM_SOLDER_EXT
-cp ../CAM/bottom_copper.grb skunk_v5.$BOTTOM_COPPER_EXT
-cp ../CAM/inner_copper_2.grb skunk_v5.$INNER_2_EXT
-cp ../CAM/inner_copper_1.grb skunk_v5.$INNER_1_EXT
-cp ../CAM/top_copper.grb skunk_v5.$TOP_COPPER_EXT
-cp ../CAM/top_solder_mask.grb skunk_v5.$TOP_SOLDER_EXT
-cp ../CAM/top_silk.grb skunk_v5.$TOP_SILK_EXT
-cp ../CAM/drill_file.drl skunk_v5.$DRILL_EXT
-cp ../CAM/board_outline.grb skunk_v5.$BOARD_OUTLINE_EXT
-rm -f skunk_v5.zip
-zip skunk_v5.zip skunk_v5.$BOTTOM_SILK_EXT
-zip skunk_v5.zip skunk_v5.$BOTTOM_SOLDER_EXT
-zip skunk_v5.zip skunk_v5.$BOTTOM_COPPER_EXT
-zip skunk_v5.zip skunk_v5.$INNER_2_EXT
-zip skunk_v5.zip skunk_v5.$INNER_1_EXT
-zip skunk_v5.zip skunk_v5.$TOP_COPPER_EXT
-zip skunk_v5.zip skunk_v5.$TOP_SOLDER_EXT
-zip skunk_v5.zip skunk_v5.$TOP_SILK_EXT
-zip skunk_v5.zip skunk_v5.$DRILL_EXT
-zip skunk_v5.zip skunk_v5.$BOARD_OUTLINE_EXT
+sed -e "s/%REVISION%/$REVISION/" \
+	-e "s/%BOTTOM_SILK_EXT%/$BOTTOM_SILK_EXT/" \
+	-e "s/%BOTTOM_SOLDER_EXT%/$BOTTOM_SOLDER_EXT/" \
+	-e "s/%BOTTOM_COPPER_EXT%/$BOTTOM_COPPER_EXT/" \
+	-e "s/%INNER_2_EXT%/$INNER_2_EXT/" \
+	-e "s/%INNER_1_EXT%/$INNER_1_EXT/" \
+	-e "s/%TOP_COPPER_EXT%/$TOP_COPPER_EXT/" \
+	-e "s/%TOP_SOLDER_EXT%/$TOP_SOLDER_EXT/" \
+	-e "s/%TOP_SILK_EXT%/$TOP_SILK_EXT/" \
+	-e "s/%DRILL_EXT%/$DRILL_EXT/" \
+	-e "s/%BOARD_OUTLINE_EXT%/$BOARD_OUTLINE_EXT/" \
+	../readme.template.txt > readme.txt
+	
+cp "$BOTTOM_SILK_SRC" skunk_v$REVISION.$BOTTOM_SILK_EXT
+cp ../CAM/bottom_solder_mask.grb skunk_v$REVISION.$BOTTOM_SOLDER_EXT
+cp ../CAM/bottom_copper.grb skunk_v$REVISION.$BOTTOM_COPPER_EXT
+cp ../CAM/inner_copper_2.grb skunk_v$REVISION.$INNER_2_EXT
+cp ../CAM/inner_copper_1.grb skunk_v$REVISION.$INNER_1_EXT
+cp ../CAM/top_copper.grb skunk_v$REVISION.$TOP_COPPER_EXT
+cp ../CAM/top_solder_mask.grb skunk_v$REVISION.$TOP_SOLDER_EXT
+cp ../CAM/top_silk.grb skunk_v$REVISION.$TOP_SILK_EXT
+cp ../CAM/drill_file.drl skunk_v$REVISION.$DRILL_EXT
+cp ../CAM/board_outline.grb skunk_v$REVISION.$BOARD_OUTLINE_EXT
+rm -f skunk_v$REVISION.zip
+zip skunk_v$REVISION.zip skunk_v$REVISION.$BOTTOM_SILK_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$BOTTOM_SOLDER_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$BOTTOM_COPPER_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$INNER_2_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$INNER_1_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$TOP_COPPER_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$TOP_SOLDER_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$TOP_SILK_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$DRILL_EXT
+zip skunk_v$REVISION.zip skunk_v$REVISION.$BOARD_OUTLINE_EXT
---
+++ readme.template.txt
@@ -0,0 +1,47 @@
+Customer:	James Jones
+Part number:  	skunk
+Revision:	%REVISION%
+
+Table of Contents:
+
+1) File Manifest
+2) PCB Requirements
+
+1) List of files in skunk_%REVISION%.zip:
+
+	skunk_v%REVISION%.%TOP_COPPER_EXT% - Gerber/RS274X format top copper layer (component side/layer 1)
+	skunk_v%REVISION%.%TOP_SILK_EXT% - Gerber/RS274X format top silk screen (component side)
+	skunk_v%REVISION%.%TOP_SOLDER_EXT% - Gerber/RS274X format top solder mask (component side)
+
+	skunk_v%REVISION%.%INNER_1_EXT% - Gerber/RS274X format inner layer 1 (GND/layer 2)
+	skunk_v%REVISION%.%INNER_2_EXT% - Gerber/RS274X format inner layer 2 (VCC/layer 3)
+
+	skunk_v%REVISION%.%BOTTOM_COPPER_EXT% - Gerber/RS274X format bottom copper layer (secondary side/layer 4)
+	skunk_v%REVISION%.%BOTTOM_SOLDER_EXT% - Gerber/RS274X format bottom solder mask (secondary side)
+	skunk_v%REVISION%.%BOTTOM_SILK_EXT% - Gerber/RS274X format bottom silk screen (secondary side)
+
+	skunk_v%REVISION%.%BOARD_OUTLINE_EXT% - Gerber/RS274X format board outline (for routing)
+
+	skunk_v%REVISION%.%DRILL_EXT% - NC Drill File
+		NOTE:  Specified drill sizes are for Finished/Plated Hole sizes
+	
+2) PCB Requirements
+
+	Min copper line/space:	0.2mm/0.2mm	(7.875mil)
+	Min copper thickness:	0.5 ounce outer layers, 1 ounce inner layers
+
+	Finished hole sizes:	0.020", 0.039", 0.090", .0906"
+
+        Total hole count:	161
+
+	Board stack-up:		4 layer, .062" thick, standard construction
+
+	Board size:		94mm x 37.5 mm (3.702" x 1.476")
+				NOTE:  See skunk_v%REVISION%.%BOARD_OUTLINE_EXT% for outline
+
+	Board finish:		ENIG
+
+	Finger size:		82mm x 7.62mm / 624mm^2 (3.228" x 0.3" / 0.9684 in^2)
+
+	Finger finish:		Hard Gold with 30 degree bevel
+				NOTE:  DO NOT USE TIE-BAR EDGE PLATING
